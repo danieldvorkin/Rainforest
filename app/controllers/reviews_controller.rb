c0d5321a3308467da522c0e326127f5d97de0c6d
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-20 19:43:44 -0400

    Added more navigation and functionality|| Still needs to be fixed and updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,12 @@ class ReviewsController < ApplicationController
 	before_filter :ensure_logged_in, only: [:create, :destroy]
 
   def show
+    @product = load_product
   	@review = Review.find(params[:id])
   end
 
   def create
+    @product = load_product
   	@review = @product.reviews.build(review_params)
   	@review.user = current_user
 
@@ -27,6 +29,6 @@ class ReviewsController < ApplicationController
   end
 
   def load_product
-  	@product = Product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
   end
 end
