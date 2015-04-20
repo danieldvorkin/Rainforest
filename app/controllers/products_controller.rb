fe6e70fe777b712c97d2962a8a0185baa3c1bcb3
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-20 18:29:36 -0400

    Added better navigation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-  	@products = Product.find(params[:id])
+  	@product = Product.find(params[:id])
 
     if current_user
       @review = @product.reviews.build
