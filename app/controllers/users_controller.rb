4cfa07e3421ae6b32a7065661578bb0506ae7983
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-22 17:00:34 -0400

    Added profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
   	@user = User.new(user_params)
   	if @user.save
@@ -10,6 +18,22 @@ class UsersController < ApplicationController
   	else
   		render "new"
   	end
+  end
+
+  def update
+   @user = User.find(params[:id])
+
+   if @user.update_attributes(user_params)
+     redirect_to user_path(@user)
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
   end
 
   private
