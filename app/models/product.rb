a7423769456d7d4ca8d785ad9ef3337127442cc3
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-23 13:01:57 -0400

    Finally done all the functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	has_many :reviews
+	has_many :reviews, :dependent => :destroy
 	belongs_to :users
 
 	validates :description, :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-	has_secure_password
-
-	has_many :reviews
-	has_many :products, through: :reviews
-
-	validates :name, presence: true
+  has_secure_password
+  
+  validates :name, presence: true
+  
+  has_many :reviews
+  has_many :products, through: :reviews
+  has_many :products, foreign_key: 'created_by_id'
 end
